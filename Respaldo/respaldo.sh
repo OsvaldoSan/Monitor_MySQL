b6232d2d26e028a632a437dd464dd5a896cc5fd8
Admin_Os <osvaldosantos823@gmail.com>
2021-01-02 22:19:00 -0600

    Ahora elimina los meses/años pasados después de hacer un respaldo profundo

--- Respaldo/respaldo.sh
+++ Respaldo/respaldo.sh
@@ -3,13 +3,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 respaldo="/root/Administración/MySQL_AdminPremier/Respaldo"
 almacen="/root/Respaldos" # Dirección donde se guardarán los dump files
 export PATH
-#set -e # No recuerdo para que era
 
 # Creación de archivo profundo
 #La primera variable será el comando en fecha y la segunda el numero de cortes en cut, la tercer variable es la base de datos
 # con la que se trabaja
 # El archivo profundo son respaldos de mucho mayor tiempo a los que usualmente se tienen, si es que no se mantienen todos los respaldos por falta
 # de espacio
+# Irá borrando los del periodo anterior, a excepción de 1 que se quedará
 deep(){
 	mkdir  $almacen/$3/Archivo_profundo &> $respaldo/temp/arch.temp
 	touch $respaldo/temp/respaldo.temp
@@ -19,7 +19,7 @@ deep(){
 	# Con grep evitamos el directorio Archivo profundo
 	ls -tr $almacen/$3 | grep -v Archivo_profundo >  $respaldo/temp/respaldo.temp
 	filas=$(wc -l $respaldo/temp/respaldo.temp | cut -d '/' -f 1)
-
+	declare -i bandera=0
 	for i in $(seq $filas ) # Secuencia hasta $filas, por que evitamos el directorio Archivo_profundo
 	do
 		respaldo_tipo=$(awk "NR==$i" $respaldo/temp/respaldo.temp | cut -d '-' -f $cortes) # Se queda con la parte del nombre
@@ -28,10 +28,15 @@ deep(){
 		if [ $respaldo_tipo  -ne $tipo_act  ] # Si no es el mismo mes, año o dia. Depende $cortes
 		then
 			nombre=$(awk "NR==$i" $respaldo/temp/respaldo.temp)
-			cp $almacen/$3/$nombre $almacen/$3/Archivo_profundo/$nombre
-			#echo  $almacen/$3/$nombre
-			#rm -f $almacen/$3/$nombre
-			break
+			if [ $bandera -eq 0 ] # Solo guardara el más antiguo
+			then
+				mv $almacen/$3/$nombre $almacen/$3/Archivo_profundo/$nombre
+				bandera=$(($bandera+1))
+				echo  $almacen/$3/$nombre
+				continue
+			fi
+			echo  $almacen/$3/$nombre
+			rm -f $almacen/$3/$nombre
 		fi
 	done
 }
@@ -58,24 +63,26 @@ respaldo(){
 }
 
 # Creación de los respaldo a partir del archivo de configuración
-mkdir $almacen &> $respaldo/temp/arch.temp
-touch $respaldo/temp/resp.temp
-filas=$(wc -l $respaldo/conf_bases.txt | cut -d '/' -f 1)
-for i in $(seq $filas)
-do
-	awk "NR==$i" $respaldo/conf_bases.txt | grep '#' > $respaldo/temp/resp.temp
-	if [ $? -eq 0 ] # Si encuentra un # significa que se debe ignrar esa linea
-	then
-		continue
-	fi
-	base=$(awk "NR==$i" $respaldo/conf_bases.txt)
-	#echo $base
-	respaldo $base
-	deep m 2 $base
-done
+crear_respaldo(){
+	mkdir $almacen &> $respaldo/temp/arch.temp
+	touch $respaldo/temp/resp.temp
+	filas=$(wc -l $respaldo/conf_bases.txt | cut -d '/' -f 1)
+	for i in $(seq $filas)
+	do
+		awk "NR==$i" $respaldo/conf_bases.txt | grep '#' > $respaldo/temp/resp.temp
+		if [ $? -eq 0 ] # Si encuentra un # significa que se debe ignrar esa linea
+		then
+			continue
+		fi
+		base=$(awk "NR==$i" $respaldo/conf_bases.txt)
+		#echo $base
+		respaldo $base
+		deep Y 3 $base # Respaldo profundo por años
+		#deep m 2 $base # Respaldo profundo por meses
+	done
+}
 
 
-# Respalda meses anteriores
-#deep m 2 $base
-# Respalda años anteriores
-#deep Y 3 $base
+# Ejecución
+crear_respaldo
+
