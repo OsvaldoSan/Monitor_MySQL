3e113dfa98facd70247c3423e7abe3389be29948
Admin_Os <osvaldosantos823@gmail.com>
2021-01-02 21:29:01 -0600

    Se dejaron de seguir dos arhivos temporales y ahora si funciona respaldo en CentOS 7
    Se añadieron comentarios y se corrigieron algunas cosas, ahora el script se asegura de crear todos los directorios necesarios

--- Respaldo/respaldo.sh
+++ Respaldo/respaldo.sh
@@ -1,67 +1,78 @@
 #!/bin/bash
-PATH=/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+respaldo="/root/Administración/MySQL_AdminPremier/Respaldo"
+almacen="/root/Respaldos" # Dirección donde se guardarán los dump files
 export PATH
-#set -e
-#La primera variable será el comando en fech y la segunda el numero de cortes en cut, la tercer variable es el respaldo con el que se trabaja
+#set -e # No recuerdo para que era
+
 # Creación de archivo profundo
+#La primera variable será el comando en fecha y la segunda el numero de cortes en cut, la tercer variable es la base de datos
+# con la que se trabaja
+# El archivo profundo son respaldos de mucho mayor tiempo a los que usualmente se tienen, si es que no se mantienen todos los respaldos por falta
+# de espacio
 deep(){
-mkdir  ~/Respaldos/$3/Archivo_profundo &> ~/Administración/temp/arch
-tipo_act=$(date +%$1)
-cortes=$2
-ls ~/Respaldos/$3 >  ~/Administración/temp/respaldo
-filas=$(wc -l ~/Administración/temp/respaldo | cut -d '/' -f 1)
+	mkdir  $almacen/$3/Archivo_profundo &> $respaldo/temp/arch.temp
+	touch $respaldo/temp/respaldo.temp
+	tipo_act=$(date +%$1) # Mes
+	cortes=$2
+	# Todos los respaldo de esa base de datos que hay, listados por fecha de modificación inversa
+	# Con grep evitamos el directorio Archivo profundo
+	ls -tr $almacen/$3 | grep -v Archivo_profundo >  $respaldo/temp/respaldo.temp
+	filas=$(wc -l $respaldo/temp/respaldo.temp | cut -d '/' -f 1)
 
-for i in $(seq 2 $filas)
-do
-	respaldo_tipo=$(awk "NR==$i" ~/Administración/temp/respaldo | cut -d '-' -f $cortes)
-	if [ $respaldo_tipo  -ne $tipo_act  ]
-	then
-		#echo $(awk "NR==$i" ~/Administración/temp/respaldo)
-		nombre=$(awk "NR==$i" ~/Administración/temp/respaldo)
-		#rm -f ~/Respaldos/$nombre
-		cp ~/Respaldos/$3/$nombre ~/Respaldos/$3/Archivo_profundo/$nombre
-		echo  ~/Respaldos/$3/$nombre
-		break
-	fi
-done
-
+	for i in $(seq $filas ) # Secuencia hasta $filas, por que evitamos el directorio Archivo_profundo
+	do
+		respaldo_tipo=$(awk "NR==$i" $respaldo/temp/respaldo.temp | cut -d '-' -f $cortes) # Se queda con la parte del nombre
+		# que indica el mes en el que estan, el año o el dia, todo depende del valor de $cortes
+		# ls te da los archivos en un cierto orden, por eso se toma el primero
+		if [ $respaldo_tipo  -ne $tipo_act  ] # Si no es el mismo mes, año o dia. Depende $cortes
+		then
+			nombre=$(awk "NR==$i" $respaldo/temp/respaldo.temp)
+			cp $almacen/$3/$nombre $almacen/$3/Archivo_profundo/$nombre
+			#echo  $almacen/$3/$nombre
+			#rm -f $almacen/$3/$nombre
+			break
+		fi
+	done
 }
 
 
 respaldo(){
-	# Si el no se trata del respaldo completo $1 y $comando coincidirán
+	# Si el no se trata del respaldo completo $1 y $comando coincidirá
 	comando=$1
 	if [ "$1" = "Respaldo_Total" ]
 	then
 		comando=$"--all-databases"
 	fi
 
-	mkdir  ~/Respaldos/$1 &> ~/Administración/temp/arch
+	mkdir  $almacen/$1 &> $respaldo/temp/arch.temp
 	# Ejecutará la instrucción hasta que se complete con exito
-	mysqldump -B $comando > ~/Respaldos/$1/backup_$1_`date +%d-%m-%Y-%H:%M`.sql 
+	mysqldump -B $comando > $almacen/$1/backup_$1_`date +%d-%m-%Y-%H:%M`.sql
 
 	while [ $? -ne 0  ]
 	do
-	         mysqldump -B $comando > ~/Respaldos/$1/backup_$1_`date +%d-%m-%Y-%H:%M`.sql 
+		echo "Algo salio mal"
+	         mysqldump -B $comando > $almacen/$1/backup_$1_`date +%d-%m-%Y-%H:%M`.sql 
 	done
 
 }
 
 # Creación de los respaldo a partir del archivo de configuración
-filas=$(wc -l ~/Administración/respaldo/conf_resp.txt | cut -d '/' -f 1)
+mkdir $almacen &> $respaldo/temp/arch.temp
+touch $respaldo/temp/resp.temp
+filas=$(wc -l $respaldo/conf_bases.txt | cut -d '/' -f 1)
 for i in $(seq $filas)
 do
-	awk "NR==$i" ~/Administración/respaldo/conf_resp.txt | grep '#' > ~/Administración/temp/resp
-	if [ $? -eq 0 ]
+	awk "NR==$i" $respaldo/conf_bases.txt | grep '#' > $respaldo/temp/resp.temp
+	if [ $? -eq 0 ] # Si encuentra un # significa que se debe ignrar esa linea
 	then
-		#echo Comentario
 		continue
 	fi
-	base=$(awk "NR==$i" ~/Administración/respaldo/conf_resp.txt)
+	base=$(awk "NR==$i" $respaldo/conf_bases.txt)
 	#echo $base
 	respaldo $base
 	deep m 2 $base
-done 
+done
 
 
 # Respalda meses anteriores
